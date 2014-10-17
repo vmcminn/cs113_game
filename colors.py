3d76ab24136ca0b35f59221ff8c57a2aff511166
namedtuple <namedtuple@gmail.com>
2014-10-17 10:01:56 -0700

    Initial commit - implemented a basic game loop.

---
+++ colors.py
@@ -0,0 +1,13 @@
+#COLOR =  (RRR,		GGG,	BBB)
+
+BLACK 	= (0, 		0,	  	0)
+DGREY	= (64, 		64,	  	64)
+WHITE 	= (255,		255, 	255)
+
+RED 	= (255, 	0,	 	0)
+GREEN 	= (0, 		255,  	0)
+BLUE 	= (0, 		0, 	 	255)
+
+YELLOW 	= (255,		255, 	0)
+PURPLE 	= (255,		0, 		255)
+ORANGE 	= (255,		153, 	0)
